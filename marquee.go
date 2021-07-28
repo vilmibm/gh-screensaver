df34fe2274643a3adaccc44643089b15b4eeee56
nate smith <vilmibm@github.com>
2021-07-28 13:27:15 -0500

    start on figlet support

--- marquee.go
+++ marquee.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/lukesampson/figlet/figletlib"
 	"github.com/mattn/go-runewidth"
 )
 
@@ -9,7 +10,7 @@ type MarqueeSaver struct {
 	screen tcell.Screen
 	style  tcell.Style
 	x      int
-	msg    string
+	banner string
 	inputs map[string]string
 }
 
@@ -18,8 +19,14 @@ func NewMarqueeSaver(opts screensaverOpts) (Screensaver, error) {
 	if err := bs.Initialize(opts); err != nil {
 		return nil, err
 	}
-	bs.msg = "HELLO WORLD"
 	width, _ := bs.screen.Size()
+	// TODO vendor fonts
+	f, err := figletlib.GetFontByName("/usr/share/figlet", "script")
+	if err != nil {
+		return nil, err
+	}
+	// TODO parameterize message
+	bs.banner = figletlib.SprintMsg("HELLO WORLD", f, width, f.Settings(), "left")
 	bs.x = width
 	return bs, nil
 }
@@ -48,10 +55,15 @@ func (bs *MarqueeSaver) Update() error {
 	width, height := bs.screen.Size()
 	y := height / 2
 	bs.x--
-	if bs.x+runewidth.StringWidth(bs.msg) < 0 {
+
+	// TODO fix this to work with banners
+	// - split into lines
+	// - ensure enough room for max width line
+	// - draw str each line
+	if bs.x+runewidth.StringWidth(bs.banner) < 0 {
 		bs.x = width
 	}
 
-	drawStr(bs.screen, bs.x, y, bs.style, bs.msg)
+	drawStr(bs.screen, bs.x, y, bs.style, bs.banner)
 	return nil
 }
