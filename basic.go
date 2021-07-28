804e0f4a0ab6fcd796ae0aafdb2352b4e125e4a4
nate smith <vilmibm@github.com>
2021-07-28 11:41:46 -0500

    scroll basic message across the sky

--- basic.go
+++ basic.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/mattn/go-runewidth"
+)
 
 type BasicSaver struct {
 	screen tcell.Screen
 	style  tcell.Style
-	frame  int
+	x      int
+	msg    string
 }
 
 func NewBasicSaver(opts screensaverOpts) (Screensaver, error) {
@@ -13,6 +17,9 @@ func NewBasicSaver(opts screensaverOpts) (Screensaver, error) {
 	if err := bs.Initialize(opts); err != nil {
 		return nil, err
 	}
+	bs.msg = "HELLO WORLD"
+	width, _ := bs.screen.Size()
+	bs.x = width
 	return bs, nil
 }
 
@@ -27,9 +34,13 @@ func (bs *BasicSaver) Initialize(opts screensaverOpts) error {
 // TODO track x pos in struct
 
 func (bs *BasicSaver) Update() error {
-	bs.frame++
-	x := 0
-	y := 0
-	drawStr(bs.screen, x, y, bs.style, "HELLO WORLD")
+	width, height := bs.screen.Size()
+	y := height / 2
+	bs.x--
+	if bs.x+runewidth.StringWidth(bs.msg) < 0 {
+		bs.x = width
+	}
+
+	drawStr(bs.screen, bs.x, y, bs.style, bs.msg)
 	return nil
 }
