3922fd9549d54288a0b157bf2a9e71905cdac34e
vilmibm <vilmibm@neongrid.space>
2021-08-18 14:00:30 -0500

    WIP fireworks

--- savers/fireworks.go
+++ savers/fireworks.go
@@ -8,11 +8,17 @@ import (
 	"github.com/vilmibm/gh-screensaver/savers/shared"
 )
 
+// Want to launch fireworks from bottom of screen at a random x coord. Interval for shooting the firework with chance to fire per x coord chosen.
+// Each firework should be its own updating entity that advances up a random number of y coords bounded by screen height, removing itself onces it's exploded.
+// Fireworks should have a color pallete and then a particular explody animation.
+// Not for MVP but would be cool to have "rare" fireworks that occur way less frequently.
+
 type FireworksSaver struct {
-	screen tcell.Screen
-	style  tcell.Style
-	color  bool
-	inputs map[string]string
+	screen    tcell.Screen
+	style     tcell.Style
+	color     bool
+	inputs    map[string]string
+	fireworks []*firework
 }
 
 func NewFireworksSaver(opts shared.ScreensaverOpts) (shared.Screensaver, error) {
@@ -51,6 +57,41 @@ func (fs *FireworksSaver) SetInputs(inputs map[string]string) error {
 }
 
 func (fs *FireworksSaver) Update() error {
-	// TODO
+	// TODO track current fireworks, looping and calling Update on each one
+	// TODO potentially launch new firework
 	return nil
+}
+
+type spriteFrame string
+
+type sprite struct {
+	frames []spriteFrame
+	frame  int
+}
+
+func (s *sprite) Advance() {
+	s.frame++
+	if s.frame == len(s.frames) {
+		s.frame = 0
+	}
+}
+
+type firework struct {
+	Color1        tcell.Color
+	Color2        tcell.Color
+	ShootSprite   sprite
+	ExplodeSprite sprite
+	x             int
+	y             int
+	height        int
+	screen        tcell.Screen
+	style         tcell.Style
+}
+
+func (f *firework) Update() {
+	// TODO animate shootsprite
+	// TODO increase height
+	// TODO check height, prep to explode
+	// TODO exlode
+	// TODO remove
 }
