6f3f556d7b49bc016c4e62fdfc7474a046840ab6
vilmibm <vilmibm@github.com>
2021-08-23 19:53:46 -0500

    finally

--- savers/starfield.go
+++ savers/starfield.go
@@ -1,8 +1,10 @@
 package savers
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
@@ -28,8 +30,8 @@ type StarfieldSaver struct {
 
 	projMatrix [16]float64
 
+	speed    float64
 	maxStars int
-	interval time.Duration
 
 	stars []*star
 }
@@ -56,9 +58,8 @@ func (s *StarfieldSaver) Initialize(opts shared.ScreensaverOpts) error {
 	s.projAspect = float64(s.width) / float64(s.height) * s.fontAspect
 	s.theta = 45 * deg2rad
 
-	interval, _ := time.ParseDuration("1s")
-	s.interval = interval
-	s.maxStars = 10
+	s.speed = 4.0
+	s.maxStars = 300
 
 	s.projMatrix = [16]float64{
 		1.0 / math.Tan(s.theta*0.5) / s.projAspect,
@@ -95,22 +96,23 @@ type star struct {
 	vec []float64
 }
 
-func (s *star) Project(m [16]float64) []float64 {
-	original := []float64{}
-	for _, f := range s.vec {
-		original = append(original, f)
-	}
-	s.vec[0] = s.vec[0]*m[0] + s.vec[1]*m[4] + s.vec[2]*m[8] + s.vec[3]*m[12]
-	s.vec[1] = s.vec[0]*m[1] + s.vec[1]*m[5] + s.vec[2]*m[9] + s.vec[3]*m[13]
-	s.vec[2] = s.vec[0]*m[2] + s.vec[1]*m[6] + s.vec[2]*m[10] + s.vec[3]*m[14]
-	s.vec[3] = s.vec[0]*m[3] + s.vec[1]*m[7] + s.vec[2]*m[11] + s.vec[3]*m[15]
+func (s *star) Step(stepsize float64) {
+	s.vec[2] += stepsize
+}
+
+func (s *star) Project(m [16]float64) [4]float64 {
+	out := [4]float64{}
+	out[0] = s.vec[0]*m[0] + s.vec[1]*m[4] + s.vec[2]*m[8] + s.vec[3]*m[12]
+	out[1] = s.vec[0]*m[1] + s.vec[1]*m[5] + s.vec[2]*m[9] + s.vec[3]*m[13]
+	out[2] = s.vec[0]*m[2] + s.vec[1]*m[6] + s.vec[2]*m[10] + s.vec[3]*m[14]
+	out[3] = s.vec[0]*m[3] + s.vec[1]*m[7] + s.vec[2]*m[11] + s.vec[3]*m[15]
 
 	// "dehomogenize"
-	s.vec[0] = s.vec[0] / s.vec[3]
-	s.vec[1] = s.vec[1] / s.vec[3]
-	s.vec[2] = s.vec[2] / s.vec[3]
+	out[0] /= out[3]
+	out[1] /= out[3]
+	out[2] /= out[3]
 
-	return original
+	return out
 }
 
 func newStar(projAspect float64, f float64) *star {
@@ -125,32 +127,45 @@ func newStar(projAspect float64, f float64) *star {
 }
 
 func (s *StarfieldSaver) Update() error {
+	/*
+		newStars := []*star{}
+		for _, st := range s.stars {
+			if !(st.vec[2] > -s.n || st.vec[2] < -s.f) {
+				newStars = append(newStars, st)
+			}
+		}
+		s.stars = newStars
+	*/
 	for len(s.stars) < s.maxStars {
+		fmt.Fprintln(os.Stderr, len(s.stars))
 		s.stars = append(s.stars, newStar(s.projAspect, s.f))
 	}
 
-	starsNext := []*star{}
+	next := []*star{}
 
 	for _, st := range s.stars {
-		v := st.Project(s.projMatrix)
-		distance := v[0]*v[0] + v[1]*v[1] + v[2]*v[2]
+		projected := st.Project(s.projMatrix)
+		distance := st.vec[0]*st.vec[0] + st.vec[1]*st.vec[1] + st.vec[2]*st.vec[2]
 		c := "#"
 		if distance > 50 {
 			c = "."
 		} else if distance > 20 {
 			c = "*"
 		}
-		x := int((st.vec[0] + 1) * 0.5 * float64(s.width))
-		y := int((st.vec[1] + 1) * 0.5 * float64(s.height))
-		if x > 0 && x < s.width && y > 0 && y < s.height {
+		x := int((projected[0] + 1) * 0.5 * float64(s.width))
+		y := int((-projected[1] + 1) * 0.5 * float64(s.height))
+
+		if x > 0 && x < s.width && y > 0 && y < s.width {
 			// TODO set light or dark color based on distance
 			drawStr(s.screen, x, y, s.style, c)
-			starsNext = append(starsNext, st)
+			// TODO WAG
+			stepsize := s.speed * .04
+			st.Step(stepsize)
+			next = append(next, st)
 		}
-		time.Sleep(1)
 	}
 
-	s.stars = starsNext
+	s.stars = next
 
 	// TODO wtf with the depth calculation
 
