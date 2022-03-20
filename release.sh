9bbe35b77439561c3801549519d7dd9f607776dd
meiji163 <mysatellite99@gmail.com>
2022-03-19 17:22:39 -0700

    update manual release

--- release.sh
+++ release.sh
@@ -11,10 +11,10 @@ if [ "${tag}" == "" ]; then
 fi
 
 rm -rf dist
-GOOS=darwin GOARCH=amd64 go build -o "dist/darwin-x86_64-${tag}"
-GOOS=linux GOARCH=386 go build -o "dist/linux-i386-${tag}"
-GOOS=linux GOARCH=amd64 go build -o "dist/linux-x86_64-${tag}"
-GOOS=windows GOARCH=386 go build -o "dist/windows-i386-${tag}"
-GOOS=windows GOARCH=amd64 go build -o "dist/windows-x86_64-${tag}"
+GOOS=darwin GOARCH=amd64 go build -o "dist/darwin-amd64"
+GOOS=linux GOARCH=386 go build -o "dist/linux-386"
+GOOS=linux GOARCH=amd64 go build -o "dist/linux-amd64"
+GOOS=windows GOARCH=386 go build -o "dist/windows-386.exe"
+GOOS=windows GOARCH=amd64 go build -o "dist/windows-amd64.exe"
 
 gh release create $tag ./dist/* --title="${tag}" --notes "${tag}"
